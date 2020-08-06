7ea5aa93abc55c85c2eababd5553c2af569f8775
Aditya Gupta <slimshady9852@gmail.com>
2020-08-06 13:59:13 +0530

    Added word select

--- app/src/main/java/com/example/meaning4/findMeaning.java
+++ app/src/main/java/com/example/meaning4/findMeaning.java
@@ -9,6 +9,8 @@ import com.microsoft.azure.cognitiveservices.vision.computervision.models.*;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -26,23 +28,9 @@ public class findMeaning extends AppCompatActivity {
 
     String subscriptionKey = "4ca7b53995e041cbab318cbde301835e";
     String endpoint ="https://dictdetection.cognitiveservices.azure.com/";
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event)
-    {
-        TextView textView2 = findViewById(R.id.textView3);
-        int x = (int)event.getX();
-        int y = (int)event.getY();
-        textView2.setText(x+"   "+y);
-
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-            case MotionEvent.ACTION_MOVE:
-            case MotionEvent.ACTION_UP:
-        }
-
-        return false;
-    }
+    public String statusBarHeight = "";
+    List<List<Integer>> listOfLists = new ArrayList<>();
+    List<String> listOfWords = new ArrayList<>();
 
 
     @Override
@@ -50,6 +38,7 @@ public class findMeaning extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_meaning);
         Button button = findViewById(R.id.button98);
+        statusBarHeight = String.valueOf(getStatusBarHeight());
         ImageView imageView = findViewById(R.id.image98);
         int[] viewCoords = new int[2];
         imageView.getLocationOnScreen(viewCoords);
@@ -74,6 +63,36 @@ public class findMeaning extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        TextView textView2 = findViewById(R.id.textView3);
+        int x = (int)event.getX();
+        int y = (int) event.getY();
+        y -= getStatusBarHeight();
+        textView2.setText(x+"   "+y);
+        int i = 1;
+        for(int k = 0; k < listOfLists.size(); k++){
+            if(x > listOfLists.get(k).get(0) && x < listOfLists.get(k).get(2) ){
+                if(y > listOfLists.get(k).get(1) && y < listOfLists.get(k).get(3) ){
+                    Log.i("index", String.valueOf(k)+" "+i);
+                    TextView choosenWord = findViewById(R.id.choosenWord);
+                    choosenWord.setText(listOfWords.get(k));
+                    i+=1;
+
+                    break;
+                }
+            }
+        }
+
+        switch (event.getAction()) {
+
+            case MotionEvent.TOOL_TYPE_MOUSE:
+        }
+
+        return false;
+    }
+
     public void RecognizeTextOCRLocal(ComputerVisionClient client) {
         Log.i("a", "-----------------------------------------------");
         Log.i("a", "RECOGNIZE PRINTED TEXT");
@@ -96,12 +115,20 @@ public class findMeaning extends AppCompatActivity {
             Log.i("a", "Text angle: %1.3f\n"+ ocrResultLocal.textAngle());
             Log.i("a", "Orientation: " + ocrResultLocal.orientation());
 
+
             boolean firstWord = true;
             String res = "";
             for (OcrRegion reg : ocrResultLocal.regions()) {
 
                 for (OcrLine line : reg.lines()) {
                     for (OcrWord word : line.words()) {
+                        List<Integer> innerList = new ArrayList<>();
+                        String[] st = word.boundingBox().split(",");
+                        innerList.add(Integer.valueOf(st[0]));
+                        innerList.add(Integer.valueOf(st[1]));
+                        innerList.add(Integer.valueOf(st[0]) + Integer.valueOf(st[2]));
+                        innerList.add(Integer.valueOf(st[1]) + Integer.valueOf(st[3]));
+                        listOfLists.add(innerList);
                         if (firstWord) {
                             Log.i("a", "\nFirst word in first line is \"" + word.text()
                                     + "\" with  bounding box: " + word.boundingBox());
@@ -109,6 +136,7 @@ public class findMeaning extends AppCompatActivity {
                             Log.i("a", "\n");
                         }
                         Log.i("a", word.text() + " ");
+                        listOfWords.add(word.text());
                         res += word.text() + " ";
                     }
                     Log.i("a", "\n");
@@ -119,6 +147,16 @@ public class findMeaning extends AppCompatActivity {
         }catch (Exception e){
             Log.i("a", e.toString());
         }
+    }
+
+    public int getStatusBarHeight() {
+        int result = 0;
+        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            result = getResources().getDimensionPixelSize(resourceId);
+        }
+
+        return result;
     }
 
 }
